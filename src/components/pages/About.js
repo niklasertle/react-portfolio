8f08f458d173f4f0284ab6b44dc1572d31694b30
Niklas Ertle <hockeytertle@gmail.com>
2021-11-09 12:56:12 -0700

    Added pictures for about page. Styled page with pictures

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,18 +1,40 @@
+import nikPhoto1 from "../../assets/nik-1.jpg";
+import nikPhoto2 from "../../assets/nik-2.jpg";
+
 export default function About() {
   return (
     <div className="m-5">
       <h1 className="mb-5">About</h1>
-      <p>
-        Full Stack Web Developer with a Web Development Certificate from the
-        University of Denver with experience in HTML, CSS, and Javascript. As
-        well as a Certificate from Arapahoe Community College as a PC Certified
-        A+ Pro. Quick learner and someone who takes initiative on tasks. Accels
-        designing user interfaces to be friendly and easy to use for the user.
-        Looking to become a full stack web developer for an ambitious company
-        looking to make a difference.
-      </p>
+      <div className="row">
+        <div className="col-6">
+          <div>
+            <p>
+              Full Stack Web Developer with a Web Development Certificate from
+              the University of Denver with experience in HTML, CSS, and
+              Javascript. As well as a Certificate from Arapahoe Community
+              College as a PC Certified A+ Pro. I'm a quick learner and someone
+              who takes initiative on tasks. Accels designing user interfaces to
+              be friendly and easy to use for the user. Looking to become a full
+              stack web developer with experience.
+            </p>
+          </div>
+          <div className="text-center d-none d-lg-block">
+            <img
+              src={nikPhoto1}
+              alt="Photo of Nik"
+              width="50%"
+              className=" rounded img-fluid img-thumbnail"
+            />
+          </div>
+        </div>
+        <div className="col-6 text-center">
+          <img
+            src={nikPhoto2}
+            alt="Photo of Nik"
+            className="rounded float-right img-fluid img-thumbnail"
+          />
+        </div>
+      </div>
     </div>
   );
 }
-
-// TODO: Style page
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,7 +13,7 @@ export default function About() {
           <h3 className="text-center">Workout Tracker</h3>
           <img src={workoutTrackerSceenshot} alt='Workout tracker screenshot' width='100%'/>
           <p>This is a workout tracker app, that allows the user to track their workouts for a day, by saving carido and resistance workouts to a Mongo database.</p>
-          <p><a href="https://github.com/niklasertle/workout-tracker">GitHub</a> | <a href='https://whispering-citadel-87887.herokuapp.com/?id=61874958657a2a00162eb0c9'>Deployed App</a></p>
+          <p><a href="https://github.com/niklasertle/workout-tracker" target="_blank">GitHub</a> | <a href='https://whispering-citadel-87887.herokuapp.com/?id=61874958657a2a00162eb0c9' target="_blank">Deployed App</a></p>
           <h6>Technologoies</h6>
           <p>NodeJS | ExpressJS | MongoDB | Mongoose</p>
         </div>
@@ -21,7 +21,7 @@ export default function About() {
           <h3 className="text-center">Get Flexed</h3>
           <img src={getFlexedScreenshot} alt='Get Flexed home page' width='100%'/>
           <p>This is a workout blog that allows the user to log in and create workouts within categories. As well as like the workout to save for later.</p>
-          <p><a href="https://github.com/LasetteBrown/project-2-get-flexed-v2">GitHub</a> | <a href='#'>Deployed App</a></p>
+          <p><a href="https://github.com/LasetteBrown/project-2-get-flexed-v2" target="_blank">GitHub</a> | <a href='#' target="_blank">Deployed App</a></p>
           <h6>Technologoies</h6>
           <p>HTML | CSS | JavaScript | Bulma | NodeJS | ExpressJS | Handlebars</p>
         </div>
@@ -29,7 +29,7 @@ export default function About() {
           <h3 className="text-center">E-Commerce Backend</h3>
           <img src={ecommerceScreenshot} alt='Dataflow diagram' width='100%'/>
           <p>This is a program that was designed to simulate the backend of an e-commerce website using an SQL database.</p>
-          <p><a href="https://github.com/niklasertle/nje-ecommerce-backend">GitHub</a></p>
+          <p><a href="https://github.com/niklasertle/nje-ecommerce-backend" target="_blank">GitHub</a></p>
           <h6>Technologoies</h6>
           <p>NodeJS | ExpressJS | MySQL</p>
         </div>
